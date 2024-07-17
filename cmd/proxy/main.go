267c3ccdf6f09ea49d4d008bf13105eb865e67c9
V1RUSnya <dima.nesed@gmail.com>
2024-07-17 08:57:20 +0300

    remove

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"flag"
-	"github.com/gosnmp/gosnmp"
 	"github.com/hexqueller/SNMP-proxy/internal/config"
 	"github.com/hexqueller/SNMP-proxy/pkg/snmp"
-	"github.com/slayercat/GoSNMPServer"
-	"github.com/slayercat/GoSNMPServer/mibImps"
 	"log"
 	"sync"
 )
@@ -38,41 +35,4 @@ func main() {
 	wg.Wait()
 	snmp.SaveDataToFiles()
 	log.Println("Finished waiting")
-
-	conf := Config{
-		V3Username:                 "myV3User",
-		V3AuthenticationPassphrase: "myAuthPassphrase",
-		V3PrivacyPassphrase:        "myPrivPassphrase",
-		Community:                  "myCommunity",
-	}
-	master := GoSNMPServer.MasterAgent{
-		Logger: GoSNMPServer.NewDefaultLogger(),
-		SecurityConfig: GoSNMPServer.SecurityConfig{
-			AuthoritativeEngineBoots: 1,
-			Users: []gosnmp.UsmSecurityParameters{
-				{
-					UserName:                 conf.V3Username,
-					AuthenticationProtocol:   gosnmp.MD5,
-					PrivacyProtocol:          gosnmp.DES,
-					AuthenticationPassphrase: conf.V3AuthenticationPassphrase,
-					PrivacyPassphrase:        conf.V3PrivacyPassphrase,
-				},
-			},
-		},
-		SubAgents: []*GoSNMPServer.SubAgent{
-			{
-				CommunityIDs: []string{conf.Community},
-				OIDs:         mibImps.All(),
-			},
-		},
-	}
-	server := GoSNMPServer.NewSNMPServer(master)
-	err = server.ListenUDP("udp", "127.0.0.1:1161")
-	if err != nil {
-		log.Fatalf("Error in listen: %+v", err)
-	}
-	err = server.ServeForever()
-	if err != nil {
-		return
-	}
 }
